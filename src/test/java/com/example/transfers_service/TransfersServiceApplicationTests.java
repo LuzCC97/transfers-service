ab13d27d4ebac8954e9d203770a9669312b6d550
luz.chumacero <luzchumacerocalle@gmail.com>
2025-11-16 19:09:59 -0500

    Tests con JUnit y Mockito - parte final

--- src/test/java/com/example/transfers_service/TransfersServiceApplicationTests.java
+++ src/test/java/com/example/transfers_service/TransfersServiceApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.transfers_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 @SpringBootTest
 class TransfersServiceApplicationTests {
@@ -9,5 +11,15 @@ class TransfersServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+    @Test
+    @Timeout(10) // opcional: evita que quede colgado
+    void main_runs_without_web_server() {
+        String[] args = {
+                "--spring.main.web-application-type=none",
+                "--spring.main.banner-mode=off"
+        };
+        TransfersServiceApplication.main(args);
+        // Si no lanza excepción, se cubre SpringApplication.run(...) y el cierre del método.
+    }
 
 }
